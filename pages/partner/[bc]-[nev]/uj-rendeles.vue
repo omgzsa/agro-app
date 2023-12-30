5420970695e5cc5e1b28718a0902261c9f8a184e
omgzsa <zselloandras@gmail.com>
2023-12-30 21:53:13 +0100

    feat: new order w/ repeater and datepicker

--- pages/partner/[bc]-[nev]/uj-rendeles.vue
+++ pages/partner/[bc]-[nev]/uj-rendeles.vue
@@ -1,10 +1,34 @@
 <script setup>
 const user = ref({ name: 'Goldavis Kft.', bc: 78 });
 
-const date = ref(new Date());
+const products = [
+  'AF-4322-OC/Fit/E/Tox/AP tt 2,5',
+  'Szójadara II.o.46%',
+  'AF-4300-Tox/E/AP Tt csibe 4%',
+];
+const packagingOptions = ['Zsákos', 'Ömlesztett', 'BigBag'];
+
+// const date = ref(new Date());
+
+const state = reactive({
+  repeaterItems: [
+    {
+      id: 1,
+      product: products[0],
+      measure: 1,
+      packaging: packagingOptions[0],
+    },
+  ],
+  deliveryDate: new Date(),
+  deliveryNote: undefined,
+  deliveryName: undefined,
+  deliveryCity: undefined,
+  deliveryPostcode: undefined,
+  deliveryAddress: undefined,
+});
 
 const label = computed(() =>
-  date.value.toLocaleDateString('en-us', {
+  state.deliveryDate.toLocaleDateString('hu-hu', {
     weekday: 'long',
     year: 'numeric',
     month: 'short',
@@ -12,13 +36,37 @@ const label = computed(() =>
   })
 );
 
-const state = reactive({
-  deliveryNote: undefined,
-  deliveryName: undefined,
-  deliveryCity: undefined,
-  deliveryPostcode: undefined,
-  deliveryAddress: undefined,
-});
+function addItem() {
+  state.repeaterItems.push({
+    id: generateUniqueId(),
+    product: products[0],
+    measure: 1,
+    packaging: packagingOptions[0],
+  });
+}
+
+function updateProduct(index, value) {
+  state.repeaterItems[index].product = value;
+}
+
+function updateMeasure(index, value) {
+  state.repeaterItems[index].measure = value;
+}
+
+function updatePackaging(index, value) {
+  state.repeaterItems[index].packaging = value;
+}
+
+function removeItem(id) {
+  const index = state.repeaterItems.findIndex((item) => item.id === id);
+  if (index !== -1) {
+    state.repeaterItems.splice(index, 1);
+  }
+}
+
+function generateUniqueId() {
+  return '_' + Math.random().toString(36).slice(2, 11);
+}
 
 async function onSubmit(event) {
   // Do something with data
@@ -28,7 +76,7 @@ async function onSubmit(event) {
 
 <template>
   <div>
-    <!-- header -->
+    <!-- HEADER -->
     <div class="py-12 bg-agro-100">
       <UContainer class="flex flex-col items-start sm:items-center">
         <img
@@ -44,22 +92,50 @@ async function onSubmit(event) {
         </p>
       </UContainer>
     </div>
-    <!-- nav -->
+    <!-- NAV -->
     <NavPartner :name="user.name" :bc="user.bc" />
-    <!-- new order -->
+    <!-- NEW ORDER -->
     <UContainer class="pb-16">
       <h2 class="mb-8">Új rendelés</h2>
       <UForm :state="state" class="grid grid-cols-6 gap-6" @submit="onSubmit">
-        <div class="font-bold">// REPEATER here...</div>
-        <!-- datepicker -->
+        <!-- REPEATER -->
         <div class="col-span-full">
-          <UPopover :popper="{ placement: 'bottom-start' }">
-            <UButton icon="i-heroicons-calendar-days-20-solid" :label="label" />
+          <div
+            v-for="(item, index) in state.repeaterItems"
+            :key="item.id"
+            class="mb-4"
+          >
+            <RepeaterItem
+              :id="item.id"
+              :product="item.product"
+              :measure="item.measure"
+              :packaging="item.packaging"
+              :products="products"
+              :packagingOptions="packagingOptions"
+              @update:product="updateProduct(index, $event)"
+              @update:measure="updateMeasure(index, $event)"
+              @update:packaging="updatePackaging(index, $event)"
+              @remove="removeItem(item.id)"
+            />
+          </div>
+          <div class="flex gap-4">
+            <UButton @click="addItem">Új termék hozzáadása</UButton>
+          </div>
+        </div>
+        <!-- DATEPICKER -->
+        <div class="col-span-full">
+          <UFormGroup label="Szállítási dátum" name="deliveryDate">
+            <UPopover :popper="{ placement: 'bottom-start' }">
+              <UButton
+                icon="i-heroicons-calendar-days-20-solid"
+                :label="label"
+              />
 
-            <template #panel="{ close }">
-              <LazyDatePicker v-model="date" @close="close" />
-            </template>
-          </UPopover>
+              <template #panel="{ close }">
+                <LazyDatePicker v-model="state.deliveryDate" @close="close" />
+              </template>
+            </UPopover>
+          </UFormGroup>
         </div>
         <div class="col-span-full">
           <UFormGroup label="Szállítási megjegyzés" name="deliveryNote">
