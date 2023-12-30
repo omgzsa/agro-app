924a382a58d8bf0afd3f370698e057a58a238c73
omgzsa <zselloandras@gmail.com>
2023-12-30 11:48:12 +0100

    partnernavigation working w/ slugName

--- components/nav/partner.vue
+++ components/nav/partner.vue
@@ -1,5 +1,22 @@
 <script lang="ts" setup>
-//
+const props = defineProps({
+  name: String,
+  bc: Number,
+});
+
+const slugName = computed(() => {
+  let slug;
+  if (props.name) {
+    slug = props.name
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/[^a-zA-Z0-9]/g, '-')
+      .toLowerCase();
+  }
+  return slug;
+});
+
+// console.log(slugName.value);
 </script>
 <template>
   <UContainer>
@@ -7,7 +24,7 @@
       <ul class="flex flex-wrap items-center gap-x-4 gap-y-6">
         <li>
           <ULink
-            to="/uzletkotok"
+            :to="`/partner/${props.bc}-${slugName}`"
             active-class="text-white bg-agro-100"
             inactive-class="text-gray-800 bg-yellow-400 hover:bg-yellow-300"
             class="px-4 py-2 text-sm font-bold rounded-full"
@@ -17,7 +34,7 @@
         </li>
         <li>
           <ULink
-            to="/uzletkotok/uj-rendeles"
+            :to="`/partner/${props.bc}-${slugName}/uj-rendeles`"
             active-class="text-white bg-agro-100"
             inactive-class="text-gray-800 bg-yellow-400 hover:bg-yellow-300"
             class="px-4 py-2 text-sm font-bold rounded-full"
@@ -27,7 +44,7 @@
         </li>
         <li>
           <ULink
-            to="/uzletkotok/segitseg-nyujtas"
+            :to="`/partner/${props.bc}-${slugName}/segitseg-nyujtas`"
             active-class="text-white bg-agro-100"
             inactive-class="text-gray-800 bg-yellow-400 hover:bg-yellow-300"
             class="px-4 py-2 text-sm font-bold rounded-full"
