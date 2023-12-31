f3a36e45611b1169581dfec8855e6da9777d205a
omgzsa <zselloandras@gmail.com>
2023-12-31 22:36:59 +0100

    changed feeding data to json

--- pages/uzletkoto/[kod]-[nev]/partnereim.vue
+++ pages/uzletkoto/[kod]-[nev]/partnereim.vue
@@ -1,21 +1,22 @@
 <script setup>
+import partnerek from '@/assets/bk-partnerek.json';
 const user = ref({ name: 'Bolla Kálmán', kod: 'BK', bc: 243 });
 
 const columns = [
   {
-    key: 'bc',
+    key: 'No',
     label: 'BC azonosító',
   },
   {
-    key: 'nev',
+    key: 'Name',
     label: 'Név',
   },
   {
-    key: 'varos',
+    key: 'City',
     label: 'Város',
   },
   {
-    key: 'cim',
+    key: 'Address',
     label: 'Cím',
   },
   {
@@ -23,73 +24,10 @@ const columns = [
   },
 ];
 
-const partners = [
-  {
-    bc: 78,
-    nev: 'Goldavis Kft.',
-    varos: 'Sármellék',
-    cim: 'Ízek útja 8/1.',
-  },
-  {
-    bc: 81,
-    nev: 'Göcsejmix Kft',
-    varos: 'Ormándlak',
-    cim: 'Külterület, Hrsz. 043/4',
-  },
-  {
-    bc: 330,
-    nev: 'Jancsecz Lajos',
-    varos: 'Belezna',
-    cim: 'Kossuth Lajos út 59.',
-  },
-  {
-    bc: 379,
-    nev: 'Karát Broiler Kft.',
-    varos: 'Vasvár',
-    cim: 'Nagymákfa út 18.',
-  },
-  {
-    bc: 461,
-    nev: 'Hombár Kft.',
-    varos: 'Galambok',
-    cim: 'Major',
-  },
-  {
-    bc: 510,
-    nev: 'Horváth Zsolt',
-    varos: 'Szany',
-    cim: 'Dózsa György u. 80.',
-  },
-  {
-    bc: 603,
-    nev: 'Zalatárnok - Tófej Mg. Kft.',
-    varos: 'Zalatárnok',
-    cim: 'Petőfi u. 61.',
-  },
-  {
-    bc: 850,
-    nev: 'Varga Szárnyas Kft.',
-    varos: 'Becsehely',
-    cim: 'Kossuth L. u. 254.',
-  },
-  {
-    bc: 1175,
-    nev: 'Y Takarmányipari Kft.',
-    varos: 'Sásd',
-    cim: 'Törökmalom u. 2.',
-  },
-  {
-    bc: 1473,
-    nev: 'Szőlőfürt Mezőgazdasági Szövetkezet',
-    varos: 'Kecel',
-    cim: 'Szabadság tér 9.',
-  },
-];
-
 const query = ref('');
 
 const page = ref(1);
-const pageCount = 6;
+const pageCount = 8;
 
 const paginatedAndFilteredRows = computed(() => {
   const startIndex = (page.value - 1) * pageCount;
@@ -97,12 +35,16 @@ const paginatedAndFilteredRows = computed(() => {
   return filteredRows.value.slice(startIndex, endIndex);
 });
 
+const filteredAndPaginatedLength = computed(() => {
+  return filteredRows.value.length;
+});
+
 const filteredRows = computed(() => {
   if (!query.value) {
-    return partners;
+    return partnerek.value;
   }
 
-  return partners.filter((person) => {
+  return partnerek.value.filter((person) => {
     return Object.values(person).some((value) => {
       return String(value).toLowerCase().includes(query.value.toLowerCase());
     });
@@ -160,7 +102,7 @@ const filteredRows = computed(() => {
         <UPagination
           v-model="page"
           :page-count="pageCount"
-          :total="partners.length"
+          :total="filteredAndPaginatedLength"
         />
       </div>
     </UContainer>
