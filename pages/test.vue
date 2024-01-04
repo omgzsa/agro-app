89aeff503113afb78f902e665e79bdd0a76c8210
omgzsa <zselloandras@gmail.com>
2024-01-04 09:34:34 +0100

    test page

--- pages/test.vue
+++ pages/test.vue
@@ -1,23 +1,7 @@
-<script setup>
-const date = new Date();
-const label = computed(() =>
-  date.toLocaleDateString('hu-HU', {
-    weekday: 'long',
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-  })
-);
+<script lang="ts" setup>
+//
 </script>
 
 <template>
-  <div>
-    <UPopover :popper="{ placement: 'bottom-start' }">
-      <UButton icon="i-heroicons-calendar-days-20-solid" :label="label" />
-
-      <template #panel="{ close }">
-        <LazyDatePicker :model-value="date" @close="close" />
-      </template>
-    </UPopover>
-  </div>
+  <UContainer> // </UContainer>
 </template>
