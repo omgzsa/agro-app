7abbc5980812fd084f436cb88552640320ac8d82
omgzsa <zselloandras@gmail.com>
2024-01-05 14:06:32 +0100

    refactor: featured section w/ slot to render cards

---
+++ components/user/featured.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+//
+</script>
+<template>
+  <UContainer>
+    <div class="grid gap-4 pb-16 md:gap-8 sm:grid-cols-2 lg:grid-cols-3">
+      <slot />
+    </div>
+  </UContainer>
+</template>
