7a60c36f6243cb517bb69a12bafec72bafad5fa1
omgzsa <zselloandras@gmail.com>
2024-01-05 14:06:51 +0100

    featuredItem card with props

---
+++ components/user/featuredItem.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const { icon, title, value } = defineProps({
+  icon: String,
+  title: String,
+  value: String,
+});
+</script>
+
+<template>
+  <UCard :ui="{ divide: '', shadow: 'shadow-md' }" class="text-center">
+    <template #header>
+      <Icon :name="icon" size="40" class="mb-3 text-agro-100" />
+      <h4>{{ title }}</h4>
+      <span class="text-xs text-gray-400">Ebben a hónapban</span>
+    </template>
+
+    <span class="text-xl font-bold">{{ value }}</span>
+  </UCard>
+</template>
