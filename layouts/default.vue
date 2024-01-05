00c11a252f775bd098678b3bbba6bc5fdd28414b
omgzsa <zselloandras@gmail.com>
2024-01-05 14:08:00 +0100

    removed named slots for now

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,11 @@
-<script setup>
+<script lang="ts" setup>
 //
 </script>
 
 <template>
   <div>
+    <!-- <slot name="header" /> -->
     <slot />
+    <!-- <slot name="footer" /> -->
   </div>
 </template>
-
-<style></style>
