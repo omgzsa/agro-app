96cf3890190bbfb1f4259f446a54fe8262497ec7
omgzsa <zselloandras@gmail.com>
2024-01-06 18:44:51 +0100

    remove

--- middleware/isPartner.js
+++
@@ -1,6 +0,0 @@
-export default defineNuxtRouteMiddleware(async (to, from) => {
-  // if FROM is /partner and TO is not /partner, then abort navigation and redirect to /partner
-  if (to.path === '/uzletkoto/BK-bolla-kalman') {
-    return navigateTo('/partner/0032-agro-m-zrt-');
-  }
-});
