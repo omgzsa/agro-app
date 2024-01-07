3d12c3e1e1cc00ff44b65f1578de8f10537f4f0a
omgzsa <zselloandras@gmail.com>
2024-01-07 21:24:07 +0100

    global logout button

--- app.vue
+++ app.vue
@@ -1,8 +1,15 @@
+<script setup>
+import { useAuthStore } from '@/stores/auth';
+const auth = useAuthStore();
+</script>
+
 <template>
   <div>
     <NuxtLoadingIndicator />
     <NuxtLayout>
-      <!-- <template #header> IS SOMEBODY THERE? </template> -->
+      <template #header>
+        <UButton @click="auth.userLogout()" variant="ghost"> Logout </UButton>
+      </template>
       <NuxtPage :page-key="(route) => route.fullPath" />
       <!-- <template #footer> AND HERE?! </template> -->
     </NuxtLayout>
