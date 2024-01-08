cbf1a96f99bf4cd6e7ecdb2e7c67fd201c28309b
omgzsa <zselloandras@gmail.com>
2024-01-08 15:44:51 +0100

    readded partner pages on static route, dynamic params didnt work out w/ middleware

---
+++ pages/partner/index.vue
@@ -0,0 +1,175 @@
+<script setup>
+import { useGroupBy } from '@/composables/groupBy';
+import rendelesek from '@/assets/0032-rendelesek.json';
+
+definePageMeta({
+  // middleware: 'auth',
+});
+
+const router = useRouter();
+const directus_user = useDirectusUser();
+const base_url = useDirectusUrl();
+
+const { data: user } = await useFetch(
+  `${base_url}/users/${directus_user.value.id}?fields=first_name,last_name,ugyfel.*`
+);
+
+const { data: partner } = await useFetch(
+  `${base_url}/items/ugyfel/${user.value.data.ugyfel[0].ugyfel_No}`
+);
+
+console.log(partner.value.data);
+
+const q = ref('');
+const isOpen = ref(false);
+const page = ref(1);
+const pageCount = 12;
+const selectedEntries = ref([]);
+
+// table columns w/ keys
+const columns = [
+  {
+    key: 'id',
+    label: 'Számlaszám',
+  },
+  {
+    key: 'documentNum',
+    label: 'Rendelés szám',
+  },
+  {
+    key: 'shipmentDate',
+    label: 'Szállítási dátum',
+  },
+  {
+    key: 'actions',
+    label: 'Műveletek',
+  },
+];
+
+// table actions
+const items = (row) => [
+  [
+    {
+      label: 'Rendelés újra',
+      icon: 'i-heroicons-plus-circle-20-solid',
+      click: () => goToOrderWithParams(row.entries),
+    },
+    {
+      label: 'Megtekintés',
+      icon: 'i-heroicons-document-text-20-solid',
+      click: () => openModal(row.entries),
+    },
+  ],
+];
+
+const openModal = (entries) => {
+  selectedEntries.value = entries;
+  isOpen.value = true;
+};
+
+const goToOrderWithParams = (entries) => {
+  const entriesString = JSON.stringify(entries);
+  router.push({
+    path: `/partner/ujra-rendeles`,
+    query: { allEntries: entriesString },
+  });
+};
+
+// 1. GROUP ARRAY BY ORDER NO
+const { groupedArray } = useGroupBy(rendelesek.value, (item) => item.Order_No);
+
+// 2. FORMAT ARRAY TO BE USED IN TABLE
+const formattedArray = groupedArray.map((group) => {
+  const orderNo = group[0].Order_No;
+  const shipmentDate = group[0].Shipment_Date;
+  const documentNum = group[0].Document_No;
+  return { id: orderNo, shipmentDate, documentNum, entries: group };
+});
+
+// 3. PAGINATE THE FORMATTED ARRAY
+const paginatedAndFilteredRows = computed(() => {
+  const startIndex = (page.value - 1) * pageCount;
+  const endIndex = startIndex + pageCount;
+  return filteredRows.value.slice(startIndex, endIndex);
+});
+
+// 4. REVERSE THE FORMATTED ARRAY BY DATE
+const sortedArray = computed(() => {
+  return formattedArray.slice().sort((a, b) => {
+    return new Date(b.shipmentDate) - new Date(a.shipmentDate);
+  });
+});
+
+// 5. FILTER THE FORMATTED ARRAY BY SEARCH QUERY
+const filteredRows = computed(() => {
+  if (!q.value) {
+    return sortedArray.value;
+  }
+
+  return sortedArray.value.filter((order) => {
+    return Object.values(order).some((value) => {
+      return String(value).toLowerCase().includes(q.value.toLowerCase());
+    });
+  });
+});
+</script>
+
+<template>
+  <div>
+    <!-- header -->
+    <UserHeader
+      title="Webshop irányítópult"
+      :name="partner.data.nev"
+      :is-visible="true"
+    />
+    <!-- nav -->
+    <NavPartner />
+    <!-- orders -->
+    <UContainer class="pb-16">
+      <h2 class="mb-8">Utolsó rendelések</h2>
+      <div class="flex py-3.5 border-b border-gray-200 dark:border-gray-700">
+        <UInput v-model="q" placeholder="Megrendelés keresése" />
+      </div>
+      <UModal v-model="isOpen">
+        <div class="p-4">
+          <div v-for="entry in selectedEntries" :key="entry" class="p-2">
+            <p>
+              <span class="text-agro-100">Rendelés szám:</span>
+              {{ entry.Document_No }}
+            </p>
+            <p><span class="text-agro-100">Termék kód:</span> {{ entry.No }}</p>
+            <p>
+              <span class="text-agro-100">Termék:</span> {{ entry.Description }}
+            </p>
+            <p class="pb-2">
+              <span class="text-agro-100">Mennyiség:</span> {{ entry.Amount }}
+              <span>{{ entry.Unit_of_Measure }}</span>
+            </p>
+            <hr />
+          </div>
+        </div>
+      </UModal>
+      <UTable :columns="columns" :rows="paginatedAndFilteredRows">
+        <template #actions-data="{ row }">
+          <UDropdown :items="items(row)">
+            <UButton
+              color="primary"
+              variant="ghost"
+              icon="i-heroicons-information-circle-16-solid"
+            />
+          </UDropdown>
+        </template>
+      </UTable>
+
+      <div
+        class="flex justify-end py-3.5 border-t border-gray-200 dark:border-gray-700"
+      >
+        <UPagination
+          v-model="page"
+          :page-count="pageCount"
+          :total="filteredRows.length"
+        />
+      </div>
+    </UContainer>
+  </div>
+</template>
---
+++ pages/partner/uj-rendeles.vue
@@ -0,0 +1,243 @@
+<script setup>
+definePageMeta({
+  middleware: 'auth',
+});
+
+const router = useRouter();
+const directus_user = useDirectusUser();
+const base_url = useDirectusUrl();
+const toast = useToast();
+
+const { data: user } = await useFetch(
+  `${base_url}/users/${directus_user.value.id}?fields=first_name,last_name,ugyfel.*`
+);
+
+const { data: partner } = await useFetch(
+  `${base_url}/items/ugyfel/${user.value.data.ugyfel[0].ugyfel_No}`
+);
+
+// console.log(partner.value.data);
+
+const products = [
+  'VTP-130-SC/U Tejelő tehén 5%',
+  'AS-22972-P+/NSP malac px 3,5%',
+  'AS-1000 Amox Zero prest.',
+  'Pigipro 1 Milk Care',
+  'Kukoricacsíra zsákos (darált)',
+  'Arbocell RC Fine',
+  'Ammónium klorid',
+  'Szójahéj zsákos, darált',
+  'Takarmánysó - Ért. - zsák',
+  'VTP-110-U Tejelő tehén px.4%',
+  'Nátrium-hidrogén-karbonát',
+  'Takarmánymész 0,02 - 1,25 mm',
+  'Kukoricacsíra zsákos (darált)',
+  'AS-2499-P+/NSP Sertés hízó 1 kpx 3%',
+  'AS-5097 Tutti Babyst.',
+  'AS-24991-P/NSP Sertés hízó 2 kpx 3%',
+  'AS-5750 Zeusz kantáp BAG',
+  'AS-22972-P+/NSP malac px 3,5%',
+  'AS-3274-S BEST malac starter koncentrátum 10%',
+  'AS-1000 Amox Zero prest.',
+  'AS-2599-P/NSP Szoptató koca kpx 4%',
+  'BasDiar',
+  'AS-26991 P/NSP Vemhes koca kpx 3,5%',
+  'Szójahéj zsákos, darált',
+  'Kukoricacsíra zsákos (darált)',
+  'AS-2499-P+/NSP Sertés hízó 1 kpx 3%',
+  'AS-24991-P/NSP Sertés hízó 2 kpx 3%',
+  'AS-22972-P+/NSP malac px 3,5%',
+  'Dextróz',
+  'AS-3274-S BEST malac starter koncentrátum 10%',
+  'AS-2599-P/NSP Szoptató koca kpx 4%',
+  'AS-26991 P/NSP Vemhes koca kpx 3,5%',
+  'Szójahéj zsákos, darált',
+  'VBT-201A Borjú indító tak',
+  'Magnézium-oxid',
+  'VivaStart',
+  'Nyalósó szelénes 5 kg',
+  'Takarmánysó - Ért. - zsák',
+  'VTK-1550-Glüka Liver tejelő k',
+  'VTP-110-U Tejelő tehén px.4%',
+  'Takarmánymész 0,02 - 1,25 mm',
+  'Nyalósó szelénes 5 kg',
+  'Takarmánysó - Ért. - zsák',
+  'VBT-201A Borjú indító tak',
+  'VTP-110-U Tejelő tehén px.4%',
+  'VTK-1550-Glüka Liver tejelő k',
+  'Nátrium-hidrogén-karbonát',
+  'Takarmánymész 0,02 - 1,25 mm',
+  'AS-1000 Amox Zero prest.',
+  'AS-3274-S BEST malac starter koncentrátum 10%',
+  'Szójahéj zsákos, darált',
+  'Kukoricacsíra zsákos (darált)',
+  'Arbocell RC Fine',
+  'Ammónium klorid',
+  'AS-2499-P+/NSP Sertés hízó 1 kpx 3%',
+  'AS-24991-P/NSP Sertés hízó 2 kpx 3%',
+  'AS-5750 Zeusz kantáp BAG',
+  'Jefo Sp',
+  'AS-22972-P+/NSP malac px 3,5%',
+  'Nyalósó natúr 5 kg',
+  'VivaStart',
+  'VTP-110-U Tejelő tehén px.4%',
+  'Full-fat szója',
+];
+const packagingOptions = ['Zsákos', 'Ömlesztett', 'BigBag'];
+
+const state = reactive({
+  repeaterItems: [
+    {
+      id: 1,
+      product: products[0],
+      measure: 1,
+      packaging: packagingOptions[0],
+    },
+  ],
+  deliveryDate: new Date(),
+  deliveryNote: undefined,
+  deliveryName: undefined,
+  deliveryCity: undefined,
+  deliveryPostcode: undefined,
+  deliveryAddress: undefined,
+});
+
+state.deliveryName = partner.value.data.nev;
+state.deliveryCity = partner.value.data.varos;
+state.deliveryPostcode = partner.value.data.iranyitoszam;
+state.deliveryAddress = partner.value.data.cim;
+
+function addItem() {
+  state.repeaterItems.push({
+    id: generateUniqueId(),
+    product: products[0],
+    measure: 1,
+    packaging: packagingOptions[0],
+  });
+}
+
+function updateProduct(index, value) {
+  state.repeaterItems[index].product = value;
+}
+
+function updateMeasure(index, value) {
+  state.repeaterItems[index].measure = value;
+}
+
+function updatePackaging(index, value) {
+  state.repeaterItems[index].packaging = value;
+}
+
+function removeItem(id) {
+  const index = state.repeaterItems.findIndex((item) => item.id === id);
+  if (index !== -1) {
+    state.repeaterItems.splice(index, 1);
+  }
+}
+
+function generateUniqueId() {
+  return '_' + Math.random().toString(36).slice(2, 11);
+}
+
+async function onSubmit(event) {
+  toast.add({
+    title:
+      'Rendelését sikeresen elküldte! Feldolgozás után email értesítést kap.',
+  });
+  console.log(event.data);
+  // router.push('/partner');
+}
+</script>
+
+<template>
+  <div>
+    <!-- HEADER -->
+    <UserHeader
+      title="Webshop irányítópult"
+      :name="partner.data.nev"
+      :is-visible="true"
+    />
+    <!-- NAV -->
+    <NavPartner />
+    <!-- NEW ORDER -->
+    <UContainer class="pb-16">
+      <h2 class="mb-8">Új rendelés</h2>
+      <UAlert
+        icon="i-heroicons-information-circle-16-solid"
+        color="primary"
+        variant="solid"
+        title="Minimális rendelési összmennyiség: 1000 kg"
+        class="mb-8"
+      />
+      <UForm :state="state" class="grid grid-cols-6 gap-6" @submit="onSubmit">
+        <!-- REPEATER -->
+        <div class="col-span-full">
+          <div
+            v-for="(item, index) in state.repeaterItems"
+            :key="item.id"
+            class="mb-4"
+          >
+            <RepeaterItem
+              :id="item.id"
+              :product="item.product"
+              :measure="item.measure"
+              :packaging="item.packaging"
+              :products="products"
+              :packagingOptions="packagingOptions"
+              @update:product="updateProduct(index, $event)"
+              @update:measure="updateMeasure(index, $event)"
+              @update:packaging="updatePackaging(index, $event)"
+              @remove="removeItem(item.id)"
+            />
+          </div>
+          <div class="flex gap-4">
+            <UButton @click="addItem">Új termék hozzáadása</UButton>
+          </div>
+        </div>
+        <!-- DATEPICKER -->
+        <div class="col-span-full">
+          <UFormGroup label="Szállítási dátum" name="deliveryDate">
+            <agro-date-picker v-model="state.deliveryDate" />
+          </UFormGroup>
+        </div>
+        <div class="col-span-full">
+          <UFormGroup label="Szállítási megjegyzés" name="deliveryNote">
+            <UInput v-model="state.deliveryNote" />
+          </UFormGroup>
+        </div>
+
+        <div class="col-span-3">
+          <UFormGroup label="Szállítási név" name="deliveryName" required>
+            <UInput v-model="state.deliveryName" />
+          </UFormGroup>
+        </div>
+
+        <div class="col-span-3">
+          <UFormGroup label="Szállítási város" name="deliveryCity" required>
+            <UInput v-model="state.deliveryCity" />
+          </UFormGroup>
+        </div>
+
+        <div class="col-span-3">
+          <UFormGroup
+            label="Szállítási irányítószám"
+            name="deliveryPostcode"
+            required
+          >
+            <UInput v-model="state.deliveryPostcode" />
+          </UFormGroup>
+        </div>
+
+        <div class="col-span-3">
+          <UFormGroup label="Szállítási cím" name="deliveryAddress" required>
+            <UInput v-model="state.deliveryAddress" />
+          </UFormGroup>
+        </div>
+
+        <div class="col-span-2">
+          <UButton type="submit" size="xl"> Rendelés elküldése </UButton>
+        </div>
+      </UForm>
+    </UContainer>
+  </div>
+</template>
